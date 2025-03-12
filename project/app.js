893d8185176ed16c7b5f03e4173e9113168d1f8e
Iker <21isanchez@gmail.com>
2025-03-12 18:52:42 +0100

    #

--- project/app.js
+++ project/app.js
@@ -3,4 +3,11 @@ import express from "express";
 import dbconnection from "./dbconnection";
 // import pool
 
+const app = express();
+const PORT = 3080;
 const pool = new Pool(dbconnection);
+
+app.listen(PORT, () => console.log(`Servidor corriendo por el puerto ${PORT}.`))
+
+app.use(express.static('public'))
+
