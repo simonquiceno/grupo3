55f3b3b9fb8a495665b3f0e45ed5031b7cf914ea
Iker <21isanchez@gmail.com>
2025-03-13 16:32:44 +0100

    #

--- project/app.js
+++ project/app.js
@@ -2,10 +2,10 @@ import express from "express";
 import pg from "pg";
 // import cors from 'cors';
 import film from "./backend/routes/film.js";
+import actor from "./backend/routes/actor.js";
 
 const app = express();
 const PORT = 3080;
-
 
 // app.use(cors({ origin: '*' }));
 // app.use(express.static('public'));
@@ -13,8 +13,9 @@ app.use(express.json());
 // app.use();
                 
 app.use('/film', film);
+app.use('/actor', actor);
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo por el puerto ${PORT}.`)
+    console.log(`Servidor corriendo por el puerto ${PORT}.`);
 })
 
