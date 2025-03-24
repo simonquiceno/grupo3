f7f1f58c0028a17e1b517380d537566ed87f2278
Simon <21squiceno@ibadia.cat>
2025-03-24 20:42:07 +0100

    # ACCESO EN APP.JS

--- project/app.js
+++ project/app.js
@@ -1,21 +1,23 @@
 import express from "express";
 // import pg from "pg";
-// import cors from 'cors';
+import cors from 'cors';
 import film from "./backend/routes/film.js";
 import film from "./backend/routes/city.js";
-// import actor from "./backend/routes/actor.js";
+import inventory from "./backend/routes/inventory.js";
+import store from "./backend/routes/store.js";
 
 const app = express();
 const PORT = 3080;
 
-// app.use(cors({ origin: '*' }));
+app.use(cors({ origin: '*' }));
 // app.use(express.static('public'));
 app.use(express.json());
 
 
 app.use('/film', film);
 app.use('/city', film);
-
+app.use('/inventory', inventory);
+app.use('/store', store);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo por el puerto ${PORT}.`);
