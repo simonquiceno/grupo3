58fcea70b2dbeebd4c6fcbe024763996c29d75c6
Iker <21isanchez@gmail.com>
2025-03-24 17:05:19 +0100

    #

--- project/backend/routes/film.js
+++ project/backend/routes/film.js
@@ -1,11 +1,8 @@
 import { Router } from "express";
-import express from "express";
 import pg from "pg";
 import dbconnection from "../../dbconnection.js";
 
 const route = Router();
-
-// route.use(express.json());
 
 route.get("/", async (req, res) => {
     const pgClient = new pg.Client(dbconnection);
@@ -21,7 +18,6 @@ route.get('/:id', async (req, res) => {
     const pgClient = new pg.Client(dbconnection);
     await pgClient.connect();
 
-    // let resultMal = await pgClient.query('select * from film where film_id = '+ req.params.id +';');
     let result = await pgClient.query('select * from film where film_id = $1', [req.params.id]);
     res.json(result.rows[0]);
 
@@ -36,7 +32,6 @@ route.post('/', async (req, res) => {
 
     console.log(data);
 
-    // let result = await pgClient.query('select * from film where film_id = '+ req.params.id +';')
     let result = await pgClient.query('insert into film (title,description,release_year,language_id,original_language_id,rental_duration,rental_rate,length,replacement_cost,rating,special_features,last_update) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)',
         [data.title, data.description, data.release_year, data.language_id, data.original_language_id, data.rental_duration, data.rental_rate, data.length, data.replacement_cost, data.rating, data.special_features, data.last_update]);
     res.json({ message: 'Pelicula insertada correctamente', film_id: data});
@@ -48,7 +43,6 @@ route.put('/:id', async (req, res) => {
     const pgClient = new pg.Client(dbconnection);
     await pgClient.connect();
 
-    // let result = await pgClient.query('alter table')
     const data = req.body;
 
     let result = await pgClient.query('update film set title = $1, description = $2, release_year = $3, language_id = $4, original_language_id = $5, rental_duration = $6, rental_rate = $7, length = $8, replacement_cost = $9, rating = $10, special_features = $11, last_update = $12 WHERE film_id = $13',
