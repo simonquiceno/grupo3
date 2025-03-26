47167b4535b7781a3a85e7c5cb35bb53ef56a056
Iker <21isanchez@gmail.com>
2025-03-26 19:00:21 +0100

    #

--- project/backend/fetch/film-main.js
+++ project/backend/fetch/film-main.js
@@ -3,16 +3,24 @@ document.addEventListener('DOMContentLoaded', () => {
     let spinner = document.getElementById("spinner");
     // Body de la tabla
     let tabla = document.querySelector('#film_table tbody');
+    // Página actual
+    let currentPage = 1;
+    // Número de registros por página
+    let pageSize = 50;
     // Boton de añadir Modal
     let btnModalAñadir = document.getElementById('boton_añadir_modal');
     // Ventana Modal Crear
     let modalAñadir = document.getElementById('modalFilm');
     // Ventana Modal Actualizar
-    let modalActualizar = e.target.getElementById('modalFilmUpd');
+    let modalActualizar = document.getElementById('modalFilmUpd');
     // Boton Cerrar Modal
     let closeModalAñadir = document.getElementById('cerrarModalAñadir');
+    // Boton Cerrar Modal Actualizar
+    let closeModalActualizar = document.getElementById('cerrarModalActualizar');
     // Boton crear Film (Dentro de Modal)
     let btnCrear = document.getElementById('btnCrear');
+    // Boton actualizar Film
+    let btnActualizar = document.getElementById('btnActualizar');
     // Select añadir language
     let selectLanguage = document.getElementById('añadir_language_id');
     // Input ID Film
@@ -20,11 +28,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     // Función asíncrona para obtener todos los posts
-    async function filterTable() {
+    async function filterTable(page) {
         spinner.style.display = "block";
         try {
-            let response = await fetch("http://localhost:3080/film");
+            // let response = await fetch("http://localhost:3080/film");
+            let response = await fetch(`http://localhost:3080/film?page=${page}&pageSize=${pageSize}`);
             let films = await response.json();
+            tabla.innerHTML = '';
 
             for (let film of films) {
                 tabla.innerHTML += `
@@ -67,7 +77,26 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     // Llamamos a la función para ejecutar la petición
-    filterTable();
+    filterTable(currentPage);
+
+    /* Agregar eventos a los botones de paginación */
+    // Siguiente página
+    const nextPage = document.getElementById('nextPage');
+    nextPage.addEventListener('click', () => {
+        currentPage++;
+        filterTable(currentPage);
+    });
+
+    // Página anterior
+    const prevPage = document.getElementById('prevPage');
+    prevPage.addEventListener('click', () => {
+        if (currentPage > 1) {
+            currentPage--;
+            filterTable(currentPage);
+        } else {
+            console.log('Ya estas en la primnera página');
+        }
+    });
 
 
     // GESTIÓN DE BOTONES VENTANAS - AÑADIR
@@ -85,16 +114,19 @@ document.addEventListener('DOMContentLoaded', () => {
             "film_id": film_id.value,
             "title": title.value,
             "description": description.value,
-            "release_year": release_year.valueOf(),
-            "language": añadir_language_id.value,
-            "rental_duration": rental_duration.valueOf(),
-            "rental_rate": rental_rate.valueOf(),
-            "length": length.valueOf(),
-            "replacement_cost": replacement_cost.valueOf()
+            "release_year": isNaN(parseInt(release_year.value)) ? null : parseInt(release_year.value),
+            // "release_year": parseInt(release_year.value),
+            "language_id": isNaN(parseInt(añadir_language_id.value)) ? null : parseInt(añadir_language_id.value),
+            "rental_duration": isNaN(parseInt(rental_duration.value)) ? null : parseInt(rental_duration.value),
+            "rental_rate": isNaN(parseInt(rental_rate.value)) ? null : parseInt(rental_rate.value),
+            "length": isNaN(parseInt(length.valueOf())) ? null : parseInt(length.valueOf()),
+            "replacement_cost": isNaN(parseFloat(replacement_cost.value)) ? null : parseFloat(replacement_cost.value),
+            "rating": rating.value
         }
         await createFilm(film);
     });
 
+    // Cargar los languages
     async function loadLanguage() {
         try {
             let response = await fetch("http://localhost:3080/language");
@@ -113,6 +145,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
+    // Crear una nueva Film
     async function createFilm(film) {
         try {
             let response = await fetch('http://localhost:3080/film', {
@@ -146,8 +179,19 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Fetch Update
+    // Gestión de botones ventana - Actualizar
 
+    // Botón cerrar modal Update
+    closeModalActualizar.addEventListener('click', () => {
+        modalActualizar.close();
+    });
+
+    // Botón guardar modal Update
+    btnActualizar.addEventListener('click', () => {
+        updateInventory();
+    })
+
+    // Fetch Update
     async function loadFilmUpdate(e) {
         modalActualizar.showModal();
         let idActualizar = e.target.getAttribute('data-id');
@@ -155,33 +199,63 @@ document.addEventListener('DOMContentLoaded', () => {
         try {
             // Consulta de la Film por su ID
             let responseFilm = await fetch(`http://localhost:3080/film/${idActualizar}`)
-            let dataFilms = await responseFilm.json();
+            let dataFilm = await responseFilm.json();
+            
             // Consulta de los Lenguages
             let responseLanguage = await fetch(`http://localhost:3080/language`);
-            let dataLanguage = await responseLanguage.json();
+            let dataLanguages = await responseLanguage.json();
 
-            let selectLanguage_upd = document.getElementById('actualizar_language_id_upd');
-
-            // Cargamos las películas aquí ( para tener todo en el desplegable)
-            selectLanguage_upd.innerHTML = '<option value="" disabled>Selecciona un lengauge</option>'; // Limpia opciones
-            dataFilms.forEach(opcion => {
+            // Aquí cargamos las películas (para tener todo en el desplegable)
+            actualizar_language_id_upd.innerHTML = '<option value="" disabled>Selecciona un lengauge</option>'; // Limpia opciones
+            for (const language of dataLanguages) {
                 let optionAdd = document.createElement('option')
-                optionAdd.value = opcion.film_id;
-                optionAdd.textContent = opcion.title;
-                selectLanguage_upd.appendChild(optionAdd);
-            });
+                optionAdd.value = language.language_id;
+                optionAdd.textContent = language.name;
+                actualizar_language_id_upd.appendChild(optionAdd);
+            };
 
-            selectLanguage_upd.innerHTML = '<option value="" disabled>Selecciona un lengauge</option>'; // Limpia opciones
-            for (const film of dataFilms) {
-                let optionAdd = document.createElement('option')
-                optionAdd.value = film.language_id;
-                optionAdd.textContent = film.name;
-                selectLanguage.appendChild(optionAdd);
-            }
+            // Cargar ID Film
+            film_id_upd.value = dataFilm.film_id;
+
+            // Cargar Titulo Film
+            title_upd.value = dataFilm.title;
+
+            // Cargar Descripción Film
+            description_upd.value = dataFilm.description;
+
+            // Cargar Año de Salida Film
+            release_year_upd.value = dataFilm.release_year;
+
+            // Cargar option lenguaje seleccionado
+            actualizar_language_id_upd.innerHTML = '<option value="" disabled>Selecciona un lengauge</option>'; // Limpiar opciones
+            let optionLanguage = document.createElement('option');
+            optionLanguage.textContent = dataFilm.name;
+            optionLanguage.value = dataFilm.language_id;
+            optionLanguage.selected = true;
+            actualizar_language_id_upd.appendChild(optionLanguage);
+
+            // Cargar Duración del Alquiler Film
+            rental_duration_upd.value = dataFilm.rental_duration;
+
+            // Cargar Rate del Alquiler Film
+            rental_rate_upd.value = dataFilm.rental_rate;
+
+            // Cargar Length Film
+            length_upd.value = dataFilm.length;
+
+            // Cargar Replacement Cost Film
+            replacement_cost_upd.value = dataFilm.replacement_cost;
+
+            // Cargar option lenguaje seleccionado
+            rating_upd.innerHTML = '<option value="" disabled>Selecciona un lengauge</option>'; // Limpiar opciones
+            let optionRating = document.createElement('option');
+            optionRating.textContent = dataFilm.rating;
+            optionRating.value = dataFilm.rating;
+            optionRating.selected = true;
+            rating_upd.appendChild(optionRating);
 
         } catch (error) {
             console.log(error);
         }
-
     }
 });
--- project/backend/routes/film.js
+++ project/backend/routes/film.js
@@ -8,7 +8,13 @@ route.get("/", async (req, res) => {
     const pgClient = new pg.Client(dbconnection);
     await pgClient.connect();
 
-    let result = await pgClient.query('select film_id, title, description, release_year, language_id, name, rental_duration, rental_rate, length, replacement_cost, rating from film join language using(language_id);');
+    // Obtenemos el número de página y el tamaño de página desde la query (con valores por defecto)
+    const page = parseInt(req.query.page) || 1;  // Página actual, por defecto la 1
+    const pageSize = parseInt(req.query.pageSize) || 50;  // Número de registros por página, por defecto 20
+    const offset = (page - 1) * pageSize;  // Calculamos el "offset"
+
+    let result = await pgClient.query(`select film_id, title, description, release_year, language_id, name, rental_duration, rental_rate, length, replacement_cost, rating from film join language using(language_id) limit $1 offset $2`, [pageSize, offset]);
+
     res.json(result.rows);
 
     await pgClient.end();
@@ -18,7 +24,7 @@ route.get('/:id', async (req, res) => {
     const pgClient = new pg.Client(dbconnection);
     await pgClient.connect();
 
-    let result = await pgClient.query('select * from film where film_id = $1', [req.params.id]);
+    let result = await pgClient.query('select film_id, title, description, release_year, language_id, name, rental_duration, rental_rate, length, replacement_cost, rating from film join language using(language_id) where film_id = $1', [req.params.id]);
     res.json(result.rows[0]);
 
     await pgClient.end();
@@ -32,8 +38,8 @@ route.post('/', async (req, res) => {
 
     console.log(data);
 
-    let result = await pgClient.query('insert into film (title,description,release_year,language_id,original_language_id,rental_duration,rental_rate,length,replacement_cost,rating,special_features,last_update) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) returning *',
-        [data.title, data.description, data.release_year, data.language_id, data.original_language_id, data.rental_duration, data.rental_rate, data.length, data.replacement_cost, data.rating, data.special_features, data.last_update]);
+    let result = await pgClient.query('insert into film (title,description,release_year,language_id,rental_duration,rental_rate,length,replacement_cost,rating) values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning *',
+        [data.title, data.description, data.release_year, data.language_id, data.rental_duration, data.rental_rate, data.length, data.replacement_cost, data.rating]);
     let wrapper = {
         status: 'ok',
         data: result.rows[0]
